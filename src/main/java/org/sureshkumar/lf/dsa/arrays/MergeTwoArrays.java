ece8a5b849388f5de83da71e9eaeff39b4ae9fc0
sureshkumariiitn <sureshkumariiitn@gmail.com>
2022-07-09 13:54:48 +0530

    Created class for Merge Two arrays

---
+++ src/main/java/org/sureshkumar/lf/dsa/arrays/MergeTwoArrays.java
@@ -0,0 +1,8 @@
+package org.sureshkumar.lf.dsa.arrays;
+
+public class MergeTwoArrays {
+    public static void main(String ar[]){
+        String nsk = "NADIMPALLI SURESH KUMAR";
+        System.out.println("NSK = " + nsk);
+    }
+}
